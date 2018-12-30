1baca0ecd2760846dd84d402065f01634ad42c19
Casey Primozic <me@ameo.link>
2018-12-30 01:36:16 -0600

    Update CircleCI build script
     * Add in Rust, `wasm-opt`, `wasm-bindgen`, etc.

--- build_all.sh
+++ build_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Check if our dependency license list needs to be refreshed
-package_hash=`md5 ./package.json`
+package_hash=`md5sum ./package.json`
 if [[ $package_hash != `cat ./package_hash` ]]; then
 	echo $package_hash > ./package_hash
   echo "Generating new liceneses listing..."
-  nlf -c > ./src/licenses.csv
+  yarn gen-licenses
 fi
 
 rm -rf .cache public
@@ -14,4 +14,4 @@ cd triangles &&
 cd -
 cp ./triangles/build/* ./src
 ./opt.sh
-gatsby build
+yarn build
