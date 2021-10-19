a2051cebbb1319662e099f5f156fbf9c9b9a2ea1
Casey Primozic <me@ameo.link>
2021-10-18 17:05:23 -0700

    Add webpack retry chunk loading plugin
     * Keep seeing chunk load failures in sentry, maybe this will help.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { createFilePath } = require('gatsby-source-filesystem');
+const { RetryChunkLoadPlugin } = require('webpack-retry-chunk-load-plugin');
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
@@ -57,4 +58,23 @@ exports.onCreateWebpackConfig = ({ actions }) =>
     experiments: {
       asyncWebAssembly: true,
     },
+    plugins: [
+      new RetryChunkLoadPlugin({
+        // optional stringified function to get the cache busting query string appended to the script src
+        // if not set will default to appending the string `?cache-bust=true`
+        cacheBust: `function() {
+          return Date.now();
+        }`,
+        // optional value to set the amount of time in milliseconds before trying to load the chunk again. Default is 0
+        retryDelay: 300,
+        // optional value to set the maximum number of retries to load the chunk. Default is 1
+        maxRetries: 5,
+        // optional list of chunks to which retry script should be injected
+        // if not set will add retry script to all chunks that have webpack script loading
+        // chunks: ['chunkName'],
+        // optional code to be executed in the browser context if after all retries chunk is not loaded.
+        // if not set - nothing will happen and error will be returned to the chunk loader.
+        lastResortScript: 'window.location.reload()',
+      }),
+    ],
   });
